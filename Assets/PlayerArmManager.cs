4340a6c1b740b6c22806b783c85323b9449ce32f
UpperLeaf <tkdduq8260@gmail.com>
2021-01-22 23:45:54 +0900

    Feat : Player

---
+++ Assets/PlayerArmManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArmManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gun;
+
+    private Animator animator;
+
+    private Vector3 walkArmPos = new Vector3(0.004f, -0.01f, -2);
+    private Vector3 idleArmPos = new Vector3(0.032f, -0.061f, -2);
+
+    private Vector3 walkGunPos = new Vector3(-0.28f, 0.2f, -1f);
+    private Vector3 idleGunPos = new Vector3(-0.28f, 0f, -1f);
+
+    private
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();   
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Walk"))
+        {
+            transform.localPosition = walkArmPos;
+            gun.transform.localPosition = walkGunPos;
+        }
+        else
+        {
+            transform.localPosition = idleArmPos;
+            gun.transform.localPosition = idleGunPos;
+        }
+        animator.SetFloat("Walk", h * h + v * v);
+    }
+}
--- Assets/Scenes/Test/PlayerScript.cs
+++ Assets/Scenes/Test/PlayerScript.cs
@@ -5,21 +5,9 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviourPunCallbacks
 {
-    public GameObject _player;
-
-    public GameObject _bullet;
-
-    private float bulletspeed = 12f;
-
     private float speed = 3f;
 
-    private bool fireable = true;
-
     private bool isDead = false;
-
-    public float AttackCooltime = 0.3f;
-
-    private GameObject bulletInstance;
 
     private Rigidbody2D body;
 
@@ -27,16 +15,6 @@ public class PlayerScript : MonoBehaviourPunCallbacks
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        _player = gameObject;
-    }
-
-    IEnumerator Reload()
-    {
-        Debug.Log("reloading");
-        yield return new WaitForSeconds(AttackCooltime);
-        fireable = true;
-        Debug.Log(fireable);
-
     }
 
     void Update()
@@ -63,25 +41,5 @@ public class PlayerScript : MonoBehaviourPunCallbacks
         {
             transform.Translate(-speed * Time.deltaTime * transform.up);
         }
-
-        if (Input.GetMouseButtonUp(0) && fireable)
-        {
-            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 playerposition = transform.position;
-            Vector2 bulletvelocity = (mouse - playerposition).normalized * bulletspeed;
-            photonView.RPC("FireBullet", PhotonTargets.All, new object[] { bulletvelocity } );
-        }
-    }
-
-
-    [PunRPC]
-    public void FireBullet(Vector2 bulletvelocity)
-    {
-        fireable = false;
-        _bullet.transform.position = transform.position;
-        _bullet.GetComponent<bulletScript>()._player = _player;
-        GameObject bullet = Instantiate(_bullet);
-        bullet.GetComponent<Rigidbody2D>().velocity = bulletvelocity;
-        StartCoroutine("Reload");
     }
 }
