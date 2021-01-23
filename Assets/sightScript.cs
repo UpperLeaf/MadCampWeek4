2805c9478b13e3549843bef7b45b8340305e30f0
Jung In Rhee <ysrheee@kaist.ac.kr>
2021-01-23 22:41:52 +0900

    cone shape light added

---
+++ Assets/sightScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class sightScript : MonoBehaviour
+{
+
+    Vector3 rotate;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.RotateAroundLocal
+        GetComponent<Light2D>().pointLightInnerAngle= 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate.z = Vector2.SignedAngle(Camera.main.ScreenToWorldPoint(Input.mousePosition)- transform.position, Vector2.up);
+
+        transform.eulerAngles = -rotate;
+    }
+}
