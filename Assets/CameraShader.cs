629e954565a533696305656a014e72338f855911
Wonjun-Lee1009 <dpenguin2000@gmail.com>
2021-01-23 13:27:21 +0900

    Feat : Map

---
+++ Assets/CameraShader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraShader : MonoBehaviour
+{
+    public Camera camera;
+
+    public Shader EffectShader { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.SetReplacementShader (EffectShader, "RenderType");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
