3fe86edf5d1203ce7e91edde53f2aea0a85feeb2
Wonjun-Lee1009 <dpenguin2000@gmail.com>
2021-01-26 22:05:55 +0900

    map complete

---
+++ Assets/HideLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class HideLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Light2D[] lights;
+
+    Camera camera;
+
+
+    void Start()
+    {
+        lights = FindObjectsOfType<Light2D>();
+        camera = GetComponent<Camera>();
+    }
+
+    private void OnPreCull()
+    {
+        foreach(Light2D light in lights)
+        {
+            light.enabled = false;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
