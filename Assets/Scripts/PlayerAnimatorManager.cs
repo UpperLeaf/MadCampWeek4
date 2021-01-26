b2d6a3bae4bde33a9138c46f4fc49026aca96355
UpperLeaf <tkdduq8260@gmail.com>
2021-01-26 19:47:31 +0900

    Refator : Player Animator Mangaer

--- Assets/Scripts/PlayerAnimatorManager.cs
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -2,44 +2,24 @@
 using Photon.Pun;
 using System.Collections;
 using UnityEngine;
-public class PlayerAnimatorManager : MonoBehaviourPun
+public class PlayerAnimatorManager : AbstarctPlayerAnimatorManager
 {
-	private Animator animator;
-
-	private Vector3 dirc;
-
 	#region MonoBehaviour CallBacks
-	void Start()
+	protected override void Start()
 	{
-		animator = GetComponent<Animator>();
-		dirc = new Vector3(1, 1, 1);
+		base.Start();
 	}
 
-    private void Update()
+    protected override void Update()
     {
 		if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
 			return;
-		if (!animator)
-			return;
+		
 		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-	
+
 		if (!stateInfo.IsName("Dash"))
-			Move();
+			base.Update();
 	}
     #endregion
-
-    private void Move()
-	{
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
-
-		Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-
-		if (direction.x < 0)
-			dirc.x = 1;
-		else if (direction.x > 0)
-			dirc.x = -1;
-		transform.localScale = dirc;
-		animator.SetFloat("Walk", h * h + v * v);
-	}
+  
 }
--- Assets/bombPlayer/bombPlayerAnimatorManager.cs
+++ Assets/bombPlayer/bombPlayerAnimatorManager.cs
@@ -1,48 +1,15 @@
 ﻿using Photon.Pun;
 using UnityEngine;
-public class bombPlayerAnimatorManager : MonoBehaviourPunCallbacks
+public class BombPlayerAnimatorManager : AbstarctPlayerAnimatorManager
 {
-	#region Private Fields
-
-	[SerializeField]
-	private float directionDampTime = 0.25f;
-	Animator animator;
-
-
-	private Vector3 dirc;
-	#endregion
-
-	#region MonoBehaviour CallBacks
-	void Start()
+	protected override void Start()
 	{
-		animator = GetComponent<Animator>();
-
-
-
-		dirc = new Vector3(1, 1, 1);
+		base.Start();
 	}
-	void Update()
+	protected override void Update()
 	{
 		if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
 			return;
-		if (!animator)
-			return;
-		Move();
-	}
-	#endregion
-
-	private void Move()
-	{
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
-
-		if (h < 0)
-			dirc.x = 1;
-		else if (h > 0)
-			dirc.x = -1;
-
-		transform.localScale = dirc;
-
-		animator.SetFloat("walk", h * h + v * v);
+		base.Update();
 	}
 }
